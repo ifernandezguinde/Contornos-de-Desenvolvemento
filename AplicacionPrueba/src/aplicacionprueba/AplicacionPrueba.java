23216d2773b76394b227fc87d8bae450dcc9ca7d
ivan-portatil <ivanfernandez830@gmail.com>
2024-10-24 17:54:44 +0200

    first commit

---
+++ AplicacionPrueba/src/aplicacionprueba/AplicacionPrueba.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aplicacionprueba;
+
+/**
+ *
+ * @author ivan.fernandezguinde
+ */
+public class AplicacionPrueba {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("hwllo world");
+        System.out.println("1:" + puzzle.isOdd(1));
+        [[[[[[]]]]]]
+        {{{{{{{{{{{{{}}}}}}}}}}}}}
+        (((((((((((((((((())))))))))))))))))
+        
+    }
+    
+}
